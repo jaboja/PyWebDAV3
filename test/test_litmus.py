7d2bbfc90102972ea5e6d60da18dd425219e274e
Jakub Jagiełło <jaboja@jaboja.pl>
2023-04-11 20:42:12 +0200

    fix: test_litmus.py

--- test/test_litmus.py
+++ test/test_litmus.py
@@ -1,18 +1,19 @@
-import re
 import os
-import sys
-import time
+import re
 import shutil
+import subprocess
+import sys
 import tarfile
 import tempfile
+import time
 import unittest
-import subprocess
 from subprocess import run
 
 testdir = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, os.path.join(testdir, '..'))
-
-import pywebdav.server.server
+rootdir = os.path.normpath(os.path.join(testdir, '..'))
+sys.path.insert(0, rootdir)
+env = os.environ.copy()
+env["PYTHONPATH"] = rootdir
 
 # Run davserver
 user = 'test'
@@ -54,16 +55,20 @@ class Test(unittest.TestCase):
         proc = None
         try:
             print('Starting davserver')
-            davserver_cmd = [sys.executable, os.path.join(testdir, '..', 'pywebdav', 'server', 'server.py'), '-D',
-                             self.rundir, '-u', user, '-p', password, '-H', 'localhost', '--port', str(port)]
-            self.davserver_proc = subprocess.Popen(davserver_cmd)
+            davserver_cmd = [sys.executable, os.path.join(
+                testdir, '..', 'pywebdav', 'server', 'server.py'), '-D',
+                             self.rundir, '-u', user, '-p', password,
+                             '-H', 'localhost', '--port', str(port)]
+            self.davserver_proc = subprocess.Popen(davserver_cmd, env=env)
             # Ensure davserver has time to startup
             time.sleep(1)
 
             # Run Litmus
             print('Running litmus')
             try:
-                ret = run(["make", "URL=http://localhost:%d" % port, 'CREDS="%s %s"' % (user, password), "check"], cwd=self.litmus_dist, capture_output=True)
+                ret = run(["make", "URL=http://localhost:%d" % port,
+                           'CREDS="%s %s"' % (user, password), "check"],
+                          cwd=self.litmus_dist, capture_output=True)
                 results = ret.stdout
             except subprocess.CalledProcessError as ex:
                 results = ex.output
@@ -72,7 +77,7 @@ class Test(unittest.TestCase):
             for line in lines:
                 line = line.split('\r')[-1]
                 result.append(line)
-                if len(re.findall('^ *\d+\.', line)):
+                if len(re.findall(r'^ *\d+\.', line)):
                     assert line.endswith('pass'), line
 
         finally:
@@ -81,23 +86,24 @@ class Test(unittest.TestCase):
             print('Stopping davserver')
             self.davserver_proc.kill()
 
-
     def test_run_litmus_noauth(self):
 
         result = []
         proc = None
         try:
             print('Starting davserver')
-            davserver_cmd = [sys.executable, os.path.join(testdir, '..', 'pywebdav', 'server', 'server.py'), '-D',
-                             self.rundir, '-n', '-H', 'localhost', '--port', str(port)]
-            self.davserver_proc = subprocess.Popen(davserver_cmd)
+            davserver_cmd = [sys.executable, os.path.join(
+                testdir, '..', 'pywebdav', 'server', 'server.py'), '-D',
+                 self.rundir, '-n', '-H', 'localhost', '--port', str(port)]
+            self.davserver_proc = subprocess.Popen(davserver_cmd, env=env)
             # Ensure davserver has time to startup
             time.sleep(1)
 
             # Run Litmus
             print('Running litmus')
             try:
-                ret = run(["make", "URL=http://localhost:%d" % port, "check"], cwd=self.litmus_dist, capture_output=True)
+                ret = run(["make", "URL=http://localhost:%d" % port, "check"],
+                          cwd=self.litmus_dist, capture_output=True)
                 results = ret.stdout
                 
             except subprocess.CalledProcessError as ex:
@@ -107,7 +113,7 @@ class Test(unittest.TestCase):
             for line in lines:
                 line = line.split('\r')[-1]
                 result.append(line)
-                if len(re.findall('^ *\d+\.', line)):
+                if len(re.findall(r'^ *\d+\.', line)):
                     assert line.endswith('pass'), line
 
         finally:
@@ -115,7 +121,8 @@ class Test(unittest.TestCase):
 
             print('Stopping davserver')
             self.davserver_proc.kill()
-            
+
+
 if __name__ == "__main__":
     unittest.main()
     
